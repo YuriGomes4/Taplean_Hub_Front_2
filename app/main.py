1e2620e16a32be4c9ab69b901c667f8d6be5a745
Yuri Gomes <yurialdegomes@hotmail.com>
2024-03-27 19:38:54 -0300

    limite de acesso

--- app/main.py
+++ app/main.py
@@ -17,9 +17,56 @@ CORS(app)
 
 base_url = services.config.get('url_base')
 
-def token_required(route_func):
+pag_principal = "anuncios"
+
+def limitar_acesso(route_func):
     @functools.wraps(route_func)
     def wrapper(*args, **kwargs):
+        #token = request.cookies.get('token')
+        #if token:
+        #    response = requests.get(f'{base_url}/api/auth/verify', headers={'x-access-token': token})
+        #    if response.status_code == 200:
+        #        return route_func(*args, **kwargs)
+        #    else:
+        #        return redirect(url_for('login'))
+        #else:
+        #    return redirect(url_for('login'))
+
+        if 'uid' in session.keys():
+            session_id = str(session['uid'])
+            remote_adr = str(request.remote_addr)
+
+        
+            response = requests.get(f'{base_url}/api/v1/usuario', headers={'x-session-id': session_id, 'x-remote-adr': remote_adr})
+            if response.status_code == 200:
+                usuario = response.json()['result']
+                #codigo = usuario['conta']['assinatura']['codigo']
+                #match codigo:
+                #    case 'gratis':
+                #        if route_func.__name__.__contains__('anuncios') or route_func.__name__.__contains__('analise_mercado'):
+                #            return route_func(*args, **kwargs)
+                #        else:
+                #            return redirect(url_for(pag_principal))
+                #    case _:
+                #        return route_func(*args, **kwargs)
+                permissoes = usuario['conta']['assinatura']['permissoes']
+                rota_principal = route_func.__name__.split('__')[0]
+                if rota_principal in permissoes.keys():
+                    if permissoes[rota_principal]['acessar'] == True:
+                        return route_func(permissoes, *args, **kwargs)
+                    else:
+                        return redirect(url_for(pag_principal))
+            else:
+                return redirect(url_for('login'))
+
+        else:
+            return redirect(url_for('login'))
+        
+    return wrapper
+
+def token_required(route_func):
+    @functools.wraps(route_func)
+    def wrapper(permissoes={}, *args, **kwargs):
         #token = request.cookies.get('token')
         #if token:
         #    response = requests.get(f'{base_url}/api/auth/verify', headers={'x-access-token': token})
@@ -36,7 +83,7 @@ def token_required(route_func):
 
             response = requests.get(f'{base_url}/api/auth/verify', headers={'x-session-id': session_id, 'x-remote-adr': remote_adr})
             if response.status_code == 200:
-                return route_func(session_id, remote_adr, *args, **kwargs)
+                return route_func(session_id, remote_adr, permissoes, *args, **kwargs)
             else:
                 return redirect(url_for('login'))
         else:
@@ -46,8 +93,8 @@ def token_required(route_func):
 
 @app.route('/')
 @token_required
-def index(session_id, remote_adr):
-    return redirect(url_for('graficos'))
+def index(session_id, remote_adr, permissoes):
+    return redirect(url_for(pag_principal))
 	
 @app.route("/login")
 #@token_required
@@ -63,7 +110,7 @@ def login():
     #if token:
     #    response = requests.get(f'{base_url}/api/auth/verify', headers={'x-access-token': token})
     #    if response.status_code == 200:
-    #        return redirect(url_for('graficos'))
+    #        return redirect(url_for(pag_principal))
     #    else:
     #        pagina = "login"
     #        return render_template("login.html", base_url=base_url, pagina=pagina)
@@ -73,14 +120,15 @@ def login():
         
     response = requests.get(f'{base_url}/api/auth/verify', headers={'x-session-id': session_id, 'x-remote-adr': request.remote_addr})
     if response.status_code == 200:
-        return redirect(url_for('graficos'))
+        return redirect(url_for(pag_principal))
     else:
         pagina = "login"
         return render_template("login.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=request.remote_addr)
 
 @app.route("/graficos")
+@limitar_acesso
 @token_required
-def graficos(session_id, remote_adr):
+def graficos(session_id, remote_adr, permissoes):
     pagina = "graficos"
 
     estados_brasil = {
@@ -114,78 +162,89 @@ def graficos(session_id, remote_adr):
         "Tocantins": "TO"
     }
 
-    return render_template("principais/graficos.html", base_url=base_url, pagina=pagina, estados_brasil=estados_brasil, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/graficos.html", base_url=base_url, pagina=pagina, estados_brasil=estados_brasil, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/produtos")
+@limitar_acesso
 @token_required
-def produtos(session_id, remote_adr):
+def produtos(session_id, remote_adr, permissoes):
     pagina = "produtos"
-    return render_template("principais/produtos.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/produtos.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/produto/<string:mlb>")
+@limitar_acesso
 @token_required
-def produto(session_id, remote_adr, mlb):
+def produtos__unico(session_id, remote_adr, permissoes, mlb):
     pagina = "produtos"
-    return render_template("principais/produto.html", base_url=base_url, pagina=pagina, mlb=mlb, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/produto.html", base_url=base_url, pagina=pagina, mlb=mlb, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/anuncios")
+@limitar_acesso
 @token_required
-def anuncios(session_id, remote_adr):
+def anuncios(session_id, remote_adr, permissoes):
     pagina = "anuncios"
-    return render_template("principais/anuncios.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/anuncios.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/anuncios/pesquisar")
+@limitar_acesso
 @token_required
-def anuncios_pesquisar(session_id, remote_adr):
+def anuncios__pesquisar(session_id, remote_adr, permissoes):
     pagina = "anuncios"
-    return render_template("principais/anuncios_pesquisar.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/anuncios_pesquisar.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/analise_mercado")
+@limitar_acesso
 @token_required
-def analise_mercado(session_id, remote_adr):
+def analise_mercado(session_id, remote_adr, permissoes):
     pagina = "analise_mercado"
-    return render_template("principais/analise_mercado.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/analise_mercado.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/analise_mercado/nova")
+@limitar_acesso
 @token_required
-def nova_analise_mercado(session_id, remote_adr):
+def analise_mercado__nova(session_id, remote_adr, permissoes):
     pagina = "analise_mercado"
-    return render_template("principais/nova_analise_mercado.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/nova_analise_mercado.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/analise_mercado/<string:id_pesquisa>")
+@limitar_acesso
 @token_required
-def ver_analise_mercado(session_id, remote_adr, id_pesquisa):
+def analise_mercado__unico(session_id, remote_adr, permissoes, id_pesquisa):
     pagina = "analise_mercado"
-    return render_template("principais/ver_analise_mercado.html", base_url=base_url, pagina=pagina, id_pesquisa=id_pesquisa, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/ver_analise_mercado.html", base_url=base_url, pagina=pagina, id_pesquisa=id_pesquisa, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/analise_mercado/<string:id_pesquisa>/addprodutos")
+@limitar_acesso
 @token_required
-def analise_mercado_addprodutos(session_id, remote_adr, id_pesquisa):
+def analise_mercado__addprodutos(session_id, remote_adr, permissoes, id_pesquisa):
     pagina = "analise_mercado"
-    return render_template("principais/analise_mercado_addprodutos.html", base_url=base_url, pagina=pagina, id_pesquisa=id_pesquisa, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/analise_mercado_addprodutos.html", base_url=base_url, pagina=pagina, id_pesquisa=id_pesquisa, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/conta")
+@limitar_acesso
 @token_required
-def conta(session_id, remote_adr):
+def conta(session_id, remote_adr, permissoes):
     pagina = "conta"
-    return render_template("principais/conta.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/conta.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 #http://173.249.39.42:5050/conta/novo_vendedor/cb6e401c-f62c-4aef-9562-dc3ed60708a9
 
 @app.route("/conta/novo_vendedor/<string:public_id>")
+@limitar_acesso
 @token_required
-def novo_vendedor(session_id, remote_adr, public_id):
+def conta__novo_vendedor(session_id, remote_adr, permissoes, public_id):
     pagina = "conta"
-    return render_template("principais/novo_vendedor.html", base_url=base_url, pagina=pagina, public_id=public_id, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/novo_vendedor.html", base_url=base_url, pagina=pagina, public_id=public_id, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/administracao")
+@limitar_acesso
 @token_required
-def administracao(session_id, remote_adr):
+def administracao(session_id, remote_adr, permissoes):
     pagina = "administracao"
-    return render_template("principais/administracao.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr)
+    return render_template("principais/administracao.html", base_url=base_url, pagina=pagina, session_id=session_id, remote_adr=remote_adr, permissoes=permissoes)
 
 @app.route("/criar_conta")
-def criar_conta(session_id, remote_adr):
+def criar_conta(session_id, remote_adr, permissoes):
     email_id = request.args.get('ver')
     code = request.args.get('c')
     #pagina = "criar_conta"
